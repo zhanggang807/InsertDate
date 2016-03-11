2464e304dc3a8690d0146ca5f3f761e2fad5942b
zhanggang13 <zhanggang3@jd.com>
2016-03-11 13:07:05 +0800

    文档只读时弹框提示，修改默认快捷键ctrl+l

--- src/InsertDateAction.java
+++ src/InsertDateAction.java
@@ -4,6 +4,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,9 +18,13 @@ public class InsertDateAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent actionEvent){
         Editor editor = actionEvent.getRequiredData(CommonDataKeys.EDITOR);
+        final Document document = editor.getDocument();
+        if (!document.isWritable()){
+            Messages.showInfoMessage("This file is readonly !","Insert Date Here Plugin : ");
+            return ;
+        }
         CaretModel caretModel = editor.getCaretModel();
         final int offset = caretModel.getOffset();
-        final Document document = editor.getDocument();
         Project project = editor.getProject();
         Runnable runnable = new Runnable() {
             @Override
